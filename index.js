d7e38e9477c39cc54a9c0d053595a97178a10789
s248h176 <s248h176@gmail.com>
2022-03-07 14:24:56 -0600

    index.js update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 /* START CONTROLLERS HERE */
-
+const productsController = require('./controllers/products')
+app.use('/api/product', productsController)
 /* END CONTROLLERS HERE */
 
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
   console.log(`✅ PORT: ${app.get('port')} 🌟`);

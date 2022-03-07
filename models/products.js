fcf464fead92f4ed770c975957c7a2cbfacf2e12
s248h176 <s248h176@gmail.com>
2022-03-07 14:13:00 -0600

    Product model

--- models/products.js
+++ models/products.js
@@ -1,1 +1,13 @@
-test
+const mongoose = require("../connection");
+
+const BookmarkSchema = new mongoose.Schema({
+    brand: String,
+    title: String,
+    category: String,
+    price: Number, 
+    description: String
+});
+
+const Bookmark = mongoose.model("Bookmark", BookmarkSchema);
+
+module.exports = Bookmark;

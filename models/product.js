d8d0a2c1082e1e27dcce764cf0551bc7658ff7be
XuyenHoang <xhoang21@gmail.com>
2022-03-07 19:24:08 -0500

    added Sku and rename schema

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require('../connection')
+
+const ProductSchema = new mongoose.Schema({
+  brand: String,
+  title: String,
+  category: String,
+  price: Number,
+  description: String,
+  SKU: Number,
+})
+
+const Product = mongoose.model('Product', ProductSchema)
+
+module.exports = Product
